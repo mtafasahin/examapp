ba868826b358181476bb052261d1451349a545b7
Mustafa Sahin <mtafasahin@gmail.com>
2025-05-07 18:32:05 +0300

    appsettings

--- k6/k6-script.js
+++ k6/k6-script.js
@@ -4,8 +4,8 @@ import { Rate } from 'k6/metrics';
 
 export let options = {
   stages: [
-    { duration: '1m', target: 10 }, // Ramp up to 10 users
-    { duration: '2m', target: 10 }, // Stay at 10 users for 2 minutes
+    { duration: '30s', target: 500 }, // Ramp up to 10 users
+    { duration: '2m', target: 1500 }, // Stay at 10 users for 2 minutes
     { duration: '1m', target: 0 }, // Ramp down to 0 users
   ],
 };
@@ -15,7 +15,7 @@ export default function () {
   const params = {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'Bearer YOUR_TOKEN', // Replace with your actual token
+      Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIyMUk5UDJzMEpJZGNxM3NhZU9OWTc0U3loQllmdWRYWVRzMkphS2N0X1I0In0.eyJleHAiOjE3NDY2MjA5NjEsImlhdCI6MTc0NjYyMDY2MSwiYXV0aF90aW1lIjoxNzQ2NjIwNjYwLCJqdGkiOiJhMDU5ZGUzNS02ZGQ5LTRlNTMtYjliNi02NWVmNzM5ZjAwYjIiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjU2NzgvcmVhbG1zL2V4YW0tcmVhbG0iLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYzBjYzMyMDUtMzU1OS00MmRmLWI2NzItYzYyY2FhNWRlMjlhIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiZXhhbS1jbGllbnQiLCJzZXNzaW9uX3N0YXRlIjoiZWMxYmQ3NTEtOGVhNi00NzgwLTg1ODEtZjRlZWNjMmIxNWM3IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjU2NzgiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtZXhhbS1yZWFsbSIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iLCJTdHVkZW50Il19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInNpZCI6ImVjMWJkNzUxLThlYTYtNDc4MC04NTgxLWY0ZWVjYzJiMTVjNyIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6InN0dWRlbnQ5IGFzZCIsInByZWZlcnJlZF91c2VybmFtZSI6InN0dWRlbnQ5QGV4YW0uY29tIiwiZ2l2ZW5fbmFtZSI6InN0dWRlbnQ5IiwiZmFtaWx5X25hbWUiOiJhc2QiLCJlbWFpbCI6InN0dWRlbnQ5QGV4YW0uY29tIn0.iuUixedEEznJwu_JODJXzvzOueM-qLFI3_0sE-zTwQpTMoYYU-Hwne9YrW2DABqkdrg9ORZjTTq40Zt4tmGImX1kuzgaXNTnwoQTeiRjaHS-579x-LMuYwnAfwjBD9ro6CuxmBmVVKM3Q4SFYNqyk-mSSqgIq4DAmu8AyEVGcLvCscXFbM5HNPevAsOgf10Cct_ERXCVM3pAGOYEUP-l72mVDNPh6YtFRy-xhEwpGi7hH4Ly93acJiNBDAaI23SgnPqFXhwbG1uaGcfRlDxybTm8QboCmPbVePDUNZGhGGrNk5dt0xHvR9VwafaxlrSonRJ8rBTlOZzJBxD53T_yOQ', // Replace with your actual token
     },
   };
 
