79d8d8231c3ec3bf2d2ee935033fccce6790291c
mtafasahin <mtafasahin@gmail.com>
2026-04-11 12:22:05 +0300

    feat: translate

--- ui/src/app/pages/public/landing/discover/discover.component.ts
+++ ui/src/app/pages/public/landing/discover/discover.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { LightgalleryModule } from 'lightgallery/angular';
 
 @Component({
   selector: 'app-discover',
-  imports: [LightgalleryModule],
+  imports: [],
   templateUrl: './discover.component.html',
   styleUrls: ['./discover.component.scss'],
   standalone: true,
